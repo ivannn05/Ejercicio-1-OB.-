434c6d2a00f5a33dddb02363af34bcb9ca34c2d7
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2024-02-22 08:29:31 +0100

    no message

---
+++ src/main/webapp/inicio.js
@@ -0,0 +1,35 @@
+/*CASO 1*/
+var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4;
+  var b = 1;
+
+  
+
+document.write(a);
+  
+
+document.write(b);
+}
+
+document.write(a);
+document.write(b);
+
+
+/*CASO 2*/
+for (let i = 0; i<10; i++) {
+  
+
+document.write(i);
+}
+
+document.write(i);
+
+
+/*CASO 3*/
+    var z = 5
+    let x = z+10, y = 12;
+   
+    document.write(x + y);
